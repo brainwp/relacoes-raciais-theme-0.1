059be9821a2f260e031ece6e107bb89482064a8b
Everaldo Matias <matiaseveraldo@gmail.com>
2013-12-11 10:29:28 -0200

    Criando DIV's na page-home.php.

--- page-home.php
+++ page-home.php
@@ -7,6 +7,35 @@ get_header('home'); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+        
+        <div class="slider-menu-home">
+        </div><!-- .slider-menu-home -->
+        
+        <div class="subcontent-relacoes">
+        
+        	<div class="intro-relacoes-home">
+            </div><!-- .intro-relacoes-home -->
+            
+            <div class="logo-relacoes-home">
+            </div><!-- .logo-relacoes-home -->
+            
+            <div class="indiques-relacoes-home">
+            </div><!-- .indiques-relacoes-home -->
+        
+        </div><!-- .subcontent-relacoes -->
+        
+        <div class="subcontent-cartazes">
+        
+        	<div class="intro-cartazes-home">
+            </div><!-- .intro-cartazes-home -->
+        
+        	<div class="slider-cartazes-home">
+            </div><!-- .slider-cartazes-home -->
+            
+           	<div class="videos-home">
+            </div><!-- .videos-home -->
+        
+        </div><!-- .subcontent-cartazes -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
