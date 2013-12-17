6bac80e80c920a0207aa8ba325029e254b5c95b9
Diego Rojas <diego@brasa.art.br>
2013-12-17 08:30:45 -0200

    mudando o menu coleção para chamar as páginas com as URLs padrões

--- menu-colecao.php
+++ menu-colecao.php
@@ -1,13 +1,13 @@
 <div class="slider-menu-home">
     <div class="jimgMenu">
     <ul>
-        <li class="concurso"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/concurso-negro-e-educacao">Concurso Negro e Educação</a></li>
-        <li class="consulta"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/consulta-10-639-na-escola">Consulta 10.639 na Escola</a></li>
-        <li class="colecao"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/colecao-educacao-e-relacoes-raciais">Coleção Educação e Relações Raciais</a></li>
-        <li class="acoes"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/acoes-afirmativas">Ações Afirmativas</a></li>
-        <li class="intolerancia"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/intolerancia-religiosa">Intolerância Religiosa</a></li>
-        <li class="formacao"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/formacao-em-DH-e-relacoes-raciais">Formação em DH e Relações Raciais</a></li>
-        <li class="cor"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/a-cor-da-cultura">A Cor da Cultura</a></li>
+        <li class="concurso"><a href="<?php echo esc_url( home_url( '/?page_id=32' ) ); ?>">Concurso Negro e Educação</a></li>
+        <li class="consulta"><a href="<?php echo esc_url( home_url( '/?page_id=34' ) ); ?>">Consulta 10.639 na Escola</a></li>
+        <li class="colecao"><a href="<?php echo esc_url( home_url( '/?page_id=36' ) ); ?>">Coleção Educação e Relações Raciais</a></li>
+        <li class="acoes"><a href="<?php echo esc_url( home_url( '/?page_id=38' ) ); ?>">Ações Afirmativas</a></li>
+        <li class="intolerancia"><a href="<?php echo esc_url( home_url( '/?page_id=40' ) ); ?>">Intolerância Religiosa</a></li>
+        <li class="formacao"><a href="<?php echo esc_url( home_url( '/?page_id=42' ) ); ?>">Formação em DH e Relações Raciais</a></li>
+        <li class="cor"><a href="<?php echo esc_url( home_url( '/?page_id=44' ) ); ?>">A Cor da Cultura</a></li>
     </ul>
     </div>
 </div><!-- .slider-menu-home -->
